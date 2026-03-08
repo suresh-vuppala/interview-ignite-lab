4b858696fc4520e4924cf4986b841f4450def42e
unknown <vuppalasureshkumar@gmail.com>
2026-03-08 14:37:38 +0530

    cc

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -14,7 +14,11 @@ export function Layout({ children }: LayoutProps) {
         <AppSidebar />
         <div className="flex-1 flex flex-col min-w-0">
           <Header />
+<<<<<<< HEAD
           <main className="flex-1 overflow-auto px-8 lg:px-20 pt-4 lg:pt-24">
+=======
+          <main className="flex-1 overflow-auto">
+>>>>>>> parent of 78558da (cc)
             {children}
           </main>
         </div>
